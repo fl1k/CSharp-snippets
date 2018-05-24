86c7a8a74cbda8114cc911e17d915be3b7f9abb9
Vojin Golubovic <36790210+fl1k@users.noreply.github.com>
2018-05-24 17:03:24 +0200

    Added recursive search

---
+++ RecursiveSearch.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Recursive_Filesearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An example, search all patritions for .txt files
+            foreach (DriveInfo d in DriveInfo.GetDrives().Where(x => x.IsReady))
+            {
+                List<string> files = SearchDirectoriesFiletype(d.RootDirectory.FullName, ".txt");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            Console.WriteLine("\nDone...");
+            Console.ReadLine();
+        }
+
+        static List<string> SearchDirectoriesFilename(string path, string term)
+        {
+            List<string> files = new List<string>();
+            foreach (string file in Directory.EnumerateFiles(path).Where(x => x.Contains(term)))
+            {
+                try
+                {
+                    files.Add(file);
+                }
+                catch (Exception ex)
+                {
+                    // Console.WriteLine(ex.Message);
+                }
+            }
+            foreach (string subdir in Directory.EnumerateDirectories(path))
+            {
+                try
+                {
+                    files.AddRange(SearchDirectoriesFilename(subdir, term));
+                }
+                catch (Exception ex)
+                {
+                    // Console.WriteLine(ex.Message);
+                }
+            }
+            return files;
+        }
+
+        //
+        //
+        //
+        //
+        //
+        //
+
+        static List<string> SearchDirectoriesFiletype(string path, string term)
+        {
+            List<string> files = new List<string>();
+            foreach (string file in Directory.EnumerateFiles(path).Where(x => x.EndsWith(term)))
+            {
+                try
+                {
+                    files.Add(file);
+                }
+                catch (Exception ex)
+                {
+                    // Console.WriteLine(ex.Message);
+                }
+            }
+            foreach (string subdir in Directory.EnumerateDirectories(path))
+            {
+                try
+                {
+                    files.AddRange(SearchDirectoriesFiletype(subdir, term));
+                }
+                catch (Exception ex)
+                {
+                    // Console.WriteLine(ex.Message);
+                }
+            }
+            return files;
+        }
+    }
+}
